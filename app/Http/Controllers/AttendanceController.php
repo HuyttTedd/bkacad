3d454bb17c9f09b78f5f9b8d7165f0bcd07c7b09
Thế Huy <53602572+huykuy99@users.noreply.github.com>
2020-08-13 16:40:05 +0700

    13/8

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+class AttendanceController extends Controller
+{
+    public function view() {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/createdata', 'TestController@test');
 
+//Route::get('/createdata', 'TestController@test');
+Route::get('/xep_lop', 'ManagerController@xep_lop')->name('xep_lop');
